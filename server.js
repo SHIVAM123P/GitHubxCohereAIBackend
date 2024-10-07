a0d67b02772dc1bd10b6b67b6ecc71107a859966
shivam123p <kshivam12112000@gmail.com>
2024-10-07 13:29:51 +0530

    CORS

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
